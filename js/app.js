aa746ba51cb9559eb0d4e048aadec050e00bcfb7
emendes2 <emendes2@bitbucket.org>
2017-02-20 23:08:24 -0300

    Correcoes

--- js/app.js
+++ js/app.js
@@ -1,16 +1,13 @@
 var HTMLworks = "";
-while(data.next()){
+while(data.hasNext()){
     var work = data.works.pop();
-    HTMLworks = HTMLworks + '<figure class="col" onclick="openModal(%data.id%)" >'+
-                '<a href="'+work.view+'">'+
-                '<img src="image_src/'+work.img+'" alt="'+work.alt+'">'+
-                '</a><figcaption>';
+    HTMLworks = HTMLworks + '<figure class="col" >'+                               
+                '<a href="'+work.view+'"><img src="images_src/'+work.img+'" alt="'+work.alt+'"></a>'+
+                '<a href="'+work.link+'"><figcaption><h2>'+work.description+'<br /><em>'+work.link+'</em></h2></a>';
     if(work.mockup != undefined){
-        HTMLworks = HTMLworks +'<a href="'+work.mockup+'">View mockup</a>'
+        HTMLworks = HTMLworks +'<a href="'+work.mockup+'">View mockup</a><br />'
     }
-    HTMLworks = HTMLworks +'<a href="'+work.link+'">'+
-                '<em>'+work.description+'</em>'+
-                '</figcaption></figure>';
+    HTMLworks = HTMLworks + '</figcaption></figure>';
 }
 
 document.getElementById('main').innerHTML = HTMLworks;
