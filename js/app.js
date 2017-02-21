af2d6bc8dfc0e96b97d7d39dcff04407b2a5efcc
emendes2 <emendes2@bitbucket.org>
2017-02-20 23:31:56 -0300

    Correcoes

--- js/app.js
+++ js/app.js
@@ -2,11 +2,8 @@ var HTMLworks = "";
 while(data.hasNext()){
     var work = data.works.pop();
     HTMLworks = HTMLworks + '<figure class="col" >'+                               
-                '<a href="'+work.view+'"><img src="images_src/'+work.img+'" alt="'+work.alt+'"></a>'+
-                '<a href="'+work.link+'"><figcaption><h2>'+work.description+'<br /><em>'+work.link+'</em></h2></a>';
-    if(work.mockup != undefined){
-        HTMLworks = HTMLworks +'<a href="'+work.mockup+'">View mockup</a><br />'
-    }
+                '<a href="'+work.link+'"><img src="images_src/'+work.img+'" alt="'+work.alt+'">'+
+                '<figcaption><h2>'+work.description+'<br /><em>'+work.link+'</em></h2></a>';
     HTMLworks = HTMLworks + '</figcaption></figure>';
 }
 
