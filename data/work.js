73e54ff1119c3665de8d60ff79b79439f56f3b29
emendes2 <emendes2@bitbucket.org>
2017-02-22 03:25:38 -0300

    adicionando projetos

--- data/work.js
+++ data/work.js
@@ -1,63 +1,77 @@
 var data = {
-    "works" : [ {            
-            "view" : "../basic-page-and-css/index.html",
+    "works" : [
+        { 
             "img" : "basic-page-and-css-320x240.png",
             "description" : "Basic page and css",
             "alt" : "basic page and css",
             "link" : "https://goo.gl/r5hwi3"
-        },{            
-            "view" : "../blog-post-exemple/index.html",
-            "mockup" :  "../blog-post-exemple/web-dev-blog.png",
+        },{ 
             "img" : "blog-post-exemple-320x240.png",
             "description" : "Blog post exemple",
             "alt" : "blog post exemple",
             "link" : "https://goo.gl/WDzP52e"
-        },{            
-            "view" : "../bootstrap-landing-page/index.html",
+        },{ 
             "img" : "bootstrap-landing-page-320x240.png",
             "description" : "Bootstrap landing page",
             "alt" : "bootstrap landing page",
             "link" : "https://goo.gl/Kn1ULE"
-        },{            
-            "view" : "../card/index.html",
-            "mockup" :  "../card/design-prototype-320x240.png",
+        },{ 
             "img" : "card-320x240.png",
             "description" : "Card",
             "alt" : "card of animal",
             "link" : "https://goo.gl/Bnc6eK"
-        },{            
-            "view" : "../css-simpĺe-exemple/index.html",
+        },{ 
             "img" : "css-simpĺe-exemple-320x240.png",
             "description" : "Css simpĺe exemple",
             "alt" : "css simpĺe exemple",
             "link" : "https://goo.gl/D2upPu"
-        },{            
-            "view" : "../EncarteDigital/index.html",
+        },{ 
             "img" : "EncarteDigital-320x240.png",
             "description" : "Encarte Digital",
             "alt" : "encarte digital",
             "link" : "https://goo.gl/7XvTjK"
-        },{            
-            "view" : "../figures/index.html",
+        },{ 
             "img" : "figures-320x240.png",
             "description" : "Figures",
             "alt" : "figures project",
             "link" : "https://goo.gl/UInpCE"
-        },{            
-            "view" : "../mockup-to-article/index.html",
-            "mockup" :  "../mockup-to-article/blog-mockup.pdf",
+        },{ 
             "img" : "mockup-to-article-320x240.png",
             "description" : "Mockup to article",
             "alt" : "Mockup to article",
             "link" : "https://goo.gl/zWdkqK"
-        },{            
-            "view" : "../toplist/index.html",
+        },{ 
             "img" : "toplist-320x240.png",
             "description" : "Top list",
             "alt" : "toplist",
             "link" : "https://goo.gl/MRzbBO"
+        },{ 
+            "img" : "Responsive-320x240.png",
+            "description" : "Jornal responsive",
+            "alt" : "jornal responsive",
+            "link" : "https://goo.gl/MRzbBO"
+        },{ 
+            "img" : "Receita-html5-css3-320x240.png",
+            "description" : "Receita ",
+            "alt" : "receita",
+            "link" : "https://goo.gl/DRDfcN"
+        },{ 
+            "img" : "noticiaja-320x240.png",
+            "description" : "Noticias Já",
+            "alt" : "noticia já",
+            "link" : "https://goo.gl/MRzbBO"
+        },{ 
+            "img" : "telacolaborador-320x240.png",
+            "description" : "Formulário bootstrap",
+            "alt" : "Form bootstrap",
+            "link" : "https://goo.gl/2hlmLg"
+        }  ,{ 
+            "img" : "cliente-320x240.png",
+            "description" : "Projeto cuidador de Idosos",
+            "alt" : "cuidador de idoso",
+            "link" : "https://goo.gl/2hlmLg"
         }                     
-    ] 
+    ]  
 };
 
 data.hasNext = function () {
