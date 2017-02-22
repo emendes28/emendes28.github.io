5571645461f8b7431ea6f7154a03dfe5141e9edc
emendes2 <emendes2@bitbucket.org>
2017-02-21 23:31:42 -0300

    finalizando a 1 versão

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 var HTMLworks = "";
 while(data.hasNext()){
     var work = data.works.pop();
-     document.getElementById('jobTemplate').innerHTML.concat(
-    document.getElementById('jobTemplate').innerHTML
+    document.getElementById('jobs').innerHTML =
+    document.getElementById('jobs').innerHTML.concat(
+        document.getElementById('jobTemplate').innerHTML
             .replace("{{work.link}}",work.link).
-                replace("{{work.img}}",work.img).
-                replace("{{work.alt}}",work.alt).
-                replace("{{work.description}}",work.description).
-                replace("{{work.link}}",work.link));
+             replace("{{work.img}}",work.img).
+             replace("{{work.alt}}",work.alt).
+             replace("{{work.description}}",work.description).
+             replace("{{work.link}}",work.link));
 }
 
 
